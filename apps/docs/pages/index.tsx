bc2089497cc5fc2bf6a18e41bac791763b285d96
vadimgaidai <vadim.gaidai@icloud.com>
2023-01-29 12:08:23 +0200

    feat: Implement start template

---
+++ apps/docs/pages/index.tsx
@@ -0,0 +1,11 @@
+import { Button } from 'ui'
+import { NextPage } from 'next/types'
+
+const Docs: NextPage = () => (
+  <div>
+    <h1>Docs</h1>
+    <Button />
+  </div>
+)
+
+export default Docs
---
+++ apps/web/pages/index.tsx
@@ -0,0 +1,11 @@
+import { Button } from 'ui'
+import { NextPage } from 'next/types'
+
+const Web: NextPage = () => (
+  <div>
+    <h1>Web</h1>
+    <Button />
+  </div>
+)
+
+export default Web
